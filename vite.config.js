04295ddb65347bcad6481f0df05ffcebb8cdf80c
Cristopher Carlos Muñoz Aviles <carloscristopherma@gmail.com>
2026-07-19 18:52:31 -0500

    MindCampus 0.1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/app-psicologia-MILI/', // <-- REVISA QUE ESTÉ EXACTAMENTE ASÍ
+  base: '/MindCampus/',
 })
